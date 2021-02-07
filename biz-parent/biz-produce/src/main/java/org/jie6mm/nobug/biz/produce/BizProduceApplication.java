270d60a5342d682c392c64be52dec34573d4f331
mengshi(礞石) <mengshi@fudata.cn>
2021-02-07 17:05:51 +0800

    add biz-produce module

---
+++ biz-parent/biz-produce/src/main/java/org/jie6mm/nobug/biz/produce/BizProduceApplication.java
@@ -0,0 +1,17 @@
+package org.jie6mm.nobug.biz.produce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author JIE6MM
+ */
+@SpringBootApplication
+public class BizProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizProduceApplication.class, args);
+	}
+
+}
---
+++ biz-parent/biz-produce/src/test/java/org/jie6mm/nobug/biz/produce/BizProduceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jie6mm.nobug.biz.produce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizProduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

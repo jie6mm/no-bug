451504cf56b3ff2ff3cf8bcad289d06554f510af
mengshi(礞石) <mengshi@fudata.cn>
2021-02-07 17:51:43 +0800

    配置文件外挂

--- biz-parent/biz-produce/src/main/java/org/jie6mm/nobug/biz/produce/BizProduceApplication.java
+++ biz-parent/biz-produce/src/main/java/org/jie6mm/nobug/biz/produce/BizProduceApplication.java
@@ -1,17 +1,31 @@
 package org.jie6mm.nobug.biz.produce;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.annotation.PostConstruct;
 
 
 /**
  * @author JIE6MM
  */
 @SpringBootApplication
+@PropertySource(value = {
+		"file:${test.file-path}"
+}, encoding="UTF-8")
 public class BizProduceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BizProduceApplication.class, args);
 	}
 
+	@Value("${test.name:null}")
+	private String test;
+
+	@PostConstruct
+	public void init() {
+		System.out.println("test.name=" + test);
+	}
 }

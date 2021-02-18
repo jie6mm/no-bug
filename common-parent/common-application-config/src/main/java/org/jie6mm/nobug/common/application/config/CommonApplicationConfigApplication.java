7a874d07ff3fd2580ba2be2894320f23e99b0359
mengshi(礞石) <mengshi@fudata.cn>
2021-02-18 16:37:14 +0800

    配置依赖

---
+++ common-parent/common-application-config/src/main/java/org/jie6mm/nobug/common/application/config/CommonApplicationConfigApplication.java
@@ -0,0 +1,17 @@
+package org.jie6mm.nobug.common.application.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengshi(礞石)
+ * @date 2021/2/18 15:31
+ */
+@SpringBootApplication
+public class CommonApplicationConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplicationConfigApplication.class, args);
+    }
+
+}
